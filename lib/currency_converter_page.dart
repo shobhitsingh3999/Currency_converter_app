c15da45f46d0de8f00c2d7b7bf364874529cd477
shobhitsingh <shobhitsingh209801@gmail.com>
2024-05-28 16:41:48 +0530

    now app is ready with api intergration

--- lib/currency_converter_page.dart
+++ lib/currency_converter_page.dart
@@ -1,20 +1,4 @@
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-// class CurrencyConverterPage extends StatefulWidget {
-//   const CurrencyConverterPage({super.key});
-
-//   @override
-//   State<CurrencyConverterPage> createState() =>
-//       _CurrencyConverterPagestate<CurrencyConverterPage>();
-// }
-
-// class _CurrencyConverterPagestate extends State<CurrencyConverterPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold();
-//   }
-// }
 
 class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
@@ -29,167 +13,88 @@ class _CurrencyConverterMaterialPage
   double result = 0;
   final TextEditingController textEditingController = TextEditingController();
 
+  void cal() {
+    result = double.parse(textEditingController.text) * 81;
+    setState(() {});
+    //setState(() => result = double.parse(textEditingController.text) * 81);
+  }
+
   @override
   void initState() {
     super.initState();
-    print('rebuilt init');
   }
 
   @override
   //build function
   Widget build(BuildContext context) {
-    final border = OutlineInputBorder(
-      borderSide: const BorderSide(
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
         width: 2.0,
         style: BorderStyle.solid,
       ),
-      borderRadius: const BorderRadius.all(Radius.circular(5)),
+      borderRadius: BorderRadius.all(Radius.circular(5)),
     );
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'INR ${result.toString()}',
-            style: const TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 114, 115, 118),
+        appBar: AppBar(
+          title: const Text(
+            "CURRENCY APP ",
           ),
-          Padding(
+          backgroundColor: const Color.fromARGB(255, 207, 184, 35),
+        ),
+        body: Center(
+          child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              controller: textEditingController,
-              style: const TextStyle(color: Colors.black),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                hintText: 'ENTER YOUR AMOUNT IN USD',
-                hintStyle: const TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'INR ${result.toString()}',
+                  style: const TextStyle(
+                      fontSize: 45, fontWeight: FontWeight.bold),
                 ),
-                prefixIcon: const Icon(Icons.monetization_on),
-                prefixIconColor: Colors.black,
-                focusedBorder: border,
-                enabledBorder: border,
-              ),
-              keyboardType:
-                  const TextInputType.numberWithOptions(decimal: true),
-            ),
-          ),
-          //     //button
+                TextField(
+                  controller: textEditingController,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'ENTER YOUR AMOUNT IN USD',
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    prefixIcon: Icon(Icons.monetization_on),
+                    prefixIconColor: Colors.black,
+                    focusedBorder: border,
+                    enabledBorder: border,
+                  ),
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                ),
 
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              onPressed: () {
-                setState(() {
-                  result = double.parse(textEditingController.text) * 81;
-                });
-              },
-              child: const Text('CONVERT'),
+                //     //button
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: cal,
+                  child: const Text('CONVERT'),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class CurrencyConverterPagee extends StatelessWidget {
-//   const CurrencyConverterPagee({super.key});
-
-//   @override
-//   //build function
-//   Widget build(BuildContext context) {
-//     double result = 0;
-//     final TextEditingController textEditingController = TextEditingController();
-
-//     final border = OutlineInputBorder(
-//       borderSide: const BorderSide(
-//         width: 2.0,
-//         style: BorderStyle.solid,
-//       ),
-//       borderRadius: const BorderRadius.all(Radius.circular(5)),
-//     );
-
-//     return Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text(
-//             result.toString(),
-//             style: const TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(10.0),
-//             child: TextField(
-//               controller: textEditingController,
-//               style: const TextStyle(color: Colors.black),
-//               decoration: InputDecoration(
-//                 filled: true,
-//                 fillColor: Colors.white,
-//                 hintText: 'ENTER YOUR AMOUNT IN USD',
-//                 hintStyle: const TextStyle(
-//                   fontSize: 15,
-//                   fontWeight: FontWeight.bold,
-//                   color: Colors.black,
-//                 ),
-//                 prefixIcon: const Icon(Icons.monetization_on),
-//                 prefixIconColor: Colors.black,
-//                 focusedBorder: border,
-//                 enabledBorder: border,
-//               ),
-//               keyboardType:
-//                   const TextInputType.numberWithOptions(decimal: true),
-//             ),
-//           ),
-//           //     //button
-
-//           Padding(
-//             padding: const EdgeInsets.all(10.0),
-//             child: ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.black,
-//                 foregroundColor: Colors.white,
-//                 minimumSize: const Size(double.infinity, 50),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(5),
-//                 ),
-//               ),
-//               onPressed: () {
-//                 result = double.parse(textEditingController.text) * 81;
-//               },
-//               child: const Text('CONVERT'),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,39 @@
-//Entry Point
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/currency_converter_page.dart';
+import 'package:flutter_application_1/currency_cupertino.dart';
 
+//Entry Point
 void main() {
   runApp(const MyApp());
 }
+
 //3 type of widget
 //1. stateless
 //2. statefull
 //3. inherited
+
+//Two Design System
+//1.Material Design
+//2.Cupertino Design
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Two Design System
     //1.Material Design
-    //2.Cupertino Design
-
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 114, 115, 118),
-        appBar: AppBar(
-          title: const Text(
-            "CURRENCY APP ",
-          ),
-          backgroundColor: const Color.fromARGB(255, 207, 184, 35),
-        ),
-        body: CurrencyConverterMaterialPage(),
-      ),
-    );
+    return const MaterialApp(home: CurrencyConverterMaterialPage());
   }
 }
+
+// class MyCupertinoApp extends StatelessWidget {
+//   const MyCupertinoApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const CupertinoApp(
+//       home: CurrencyConverterCupertinopage(),
+//     );
+//   }
+// }
